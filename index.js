2e7f1d360ef0be9d7b124d9fd48a19b0f105dd62
cknoettg <cknoettg@yahoo.com>
2020-07-14 23:00:29 -0400

    Fixed package.json

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 // code away!
 const server = require('./server.js');
-// const express = require('express')
-
-// const server = express();
-
-// server.use(express.json());
-
-// server.get(`/`, (req, res) => {
-//     res.send({ message: `Server up and running` })
-// })
 
 server.listen(5000, () => {
   console.log('\n* Server Running on http://localhost:5000 *\n');
