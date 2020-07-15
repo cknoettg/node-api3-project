2bafbbff115e186846324b83e18cedff7b0e4703
cknoettg <cknoettg@yahoo.com>
2020-07-14 21:52:34 -0400

    Built out server dependencies

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const userRouter = require('./users/userRouter');
+const postRouter = require('./posts/postRouter');
 
 const server = express();
+
+server.use(helmet());
+server.use(morgan());
+server.use(express.json());
+server.use(logger);
+
+server.use('/api/users', userRouter);
+server.use('/api/post', postRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
