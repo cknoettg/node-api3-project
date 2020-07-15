d9ff8116794a0a56ad64e2751621b085919b008b
cknoettg <cknoettg@yahoo.com>
2020-07-14 22:22:38 -0400

    Finished postRouter

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -5,25 +5,60 @@ const Posts = require('./postDb');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+  Posts.get(req.query)
+  .then(posts => {
+    res.status(200).json(posts)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({ error: err.message })
+  })
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  Posts.getById(req.id)
+  .then(post => {
+    if (post) {
+      res.status(200).json(post)
+    } else {
+      res.status(404).json({error: err.message})
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({error: err.message})
+  })
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  Posts.remove(req.id)
+  .then(post => {
+    res.status(200).json(post)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(404).json({error: err.message})
+  })
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  Posts.update(req.params.id, req.body)
+  .then(post => {
+    res.status(200).json(post)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({error: err.message})
+  })
 });
 
 // custom middleware
-
 function validatePostId(req, res, next) {
-  // do your magic!
+  if(req.id) {
+    next();
+  } else {
+    res.status(400).json({message: "Invalid post id"})
+  }
 }
 
 module.exports = router;
